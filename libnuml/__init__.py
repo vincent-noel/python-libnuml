88a069499c5d0aa093591005e9549d979fd89200
vincent <vincent.noel@butantan.gov.br>
2017-08-16 13:49:40 -0400

    Adding a mock __init__.py, which redirects to libnuml.py

---
+++ libnuml/__init__.py
@@ -0,0 +1,1 @@
+from .libnuml import *
--- scripts/install.sh
+++ scripts/install.sh
@@ -21,7 +21,6 @@ cd $ROOT_DIR/builds
 ${SCRIPTS_DIR}/install_libsbml.sh
 ${SCRIPTS_DIR}/install_libnuml.sh
 
-mkdir ${ROOT_DIR}/libnuml
 cp ${ROOT_DIR}/install/lib/python2.7/site-packages/libnuml/{libnuml.py,_libnuml.so} ${ROOT_DIR}/libnuml/
 
 cd $EXEC_DIR
